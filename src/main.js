5f8f2f5c19501cfa0d44c9ec1a8ddc6150435728
Vincenzo <vincenzo.villani@studio.unibo.it>
2024-05-15 23:59:35 +0200

    better template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { cle, RenderApp } from "https://cdn.jsdelivr.net/npm/cle.js@0.15.0/lib/caged-le.js"
-import { Div } from "https://cdn.jsdelivr.net/npm/cle.js@0.15.0/extra/smart-alias.js"
+import { cle, RenderApp, html } from "https://cdn.jsdelivr.net/npm/cle.js@0.15.0/lib/caged-le.js"
+import { Div, Button } from "https://cdn.jsdelivr.net/npm/cle.js@0.15.0/extra/smart-alias.js"
+
+
+const MyButtonComponent = ({ label, onclick }) => Button({
+
+  '@input': { label },
+  
+  handle: { onclick },
+
+  class: 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+
+}, $ => $.label)
+
 
 RenderApp(document.body, cle.root(
 {
@@ -8,11 +20,20 @@ RenderApp(document.body, cle.root(
 
   {h1: "Hello World! From Cle"},
   
-  Div({}, "The count: is", $ => $.rootProp),
+  Div({}, "The count: is: ", $ => $.rootProp),
 
   html`
-    <div class="bg-red sm:hidden">
-      Hello
+    <div class="text-blue-500">
+      Hello, in tailwind style
     </div>
-  `
+  `,
+
+  MyButtonComponent({ 
+    label: "Click Me", 
+    onclick: $ => {
+      console.log('clicked');
+      $.rootProp = 0
+    }
+  }),
+
 ))
